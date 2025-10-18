c4063c04e82f5a1dd6df24354f78d054ceb54f07
Guilherme Lima <guilhermedelima@biud.com.br>
2025-10-18 20:46:32 -0300

    build: initial structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,49 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PermissionsModule } from './modules/permissions/permissions.module';
+import { PlatformsModule } from './modules/platforms/platforms.module';
+import { ProductsModule } from './modules/products/products.module';
+import { OffersModule } from './modules/offers/offers.module';
+import { IntegrationsModule } from './modules/integrations/integrations.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
+import { AffiliatesModule } from './modules/affiliates/affiliates.module';
+import { AnalyticsModule } from './modules/analytics/analytics.module';
+import { AuditModule } from './modules/audit/audit.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100,
+    }]),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    PermissionsModule,
+    PlatformsModule,
+    ProductsModule,
+    OffersModule,
+    IntegrationsModule,
+    CustomersModule,
+    OrdersModule,
+    SubscriptionsModule,
+    TransactionsModule,
+    AffiliatesModule,
+    AnalyticsModule,
+    AuditModule,
+    JobsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/modules/platforms/platforms.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { PlatformsService } from './platforms.service';
+
+@Module({
+  providers: [PlatformsService]
+})
+export class PlatformsModule {}
